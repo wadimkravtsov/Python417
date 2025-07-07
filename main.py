fa7ef8abe58e39e35c19caa4cdc71729109486b4
Вадим Кравцов <kravtsov-60@mail.ru>
2025-07-07 17:12:57 +0700

    db_11

--- main.py
+++ main.py
@@ -900,34 +900,175 @@ import sqlite3
 #     company_id INTEGER,
 #     FOREIGN KEY (company_id) REFERENCES companies(id) ON DELETE SET NULL
 #     )""")
+#
+# with sqlite3.connect("education.db") as con:
+#     cur = con.cursor()
+#     cur.execute("""CREATE TABLE IF NOT EXISTS student(
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     surname TEXT NOT NULL,
+#     name TEXT NOT NULL,
+#     patronymic  TEXT NOT NULL,
+#     age INTEGER NOT NULL CHECK(age >= 17 AND age <= 50),
+#     [group] TEXT NOT NULL,
+#     FOREIGN KEY ([group]) REFERENCES groups (id) ON DELETE RESTRICT
+#     )""")
+#
+#     cur.execute("""CREATE TABLE IF NOT EXISTS groups(
+#        id INTEGER PRIMARY KEY AUTOINCREMENT,
+#        group_name TEXT NOT NULL)
+#        """)
+#
+#     cur.execute("""CREATE TABLE IF NOT EXISTS association(
+#        lesson_id INTEGER NOT NULL,
+#        group_id INTEGER NOT NULL,
+#        PRIMARY KEY (lesson_id, group_id),
+#        FOREIGN KEY (lesson_id) REFERENCES lessons (id),
+#        FOREIGN KEY (group_id) REFERENCES groups (id)
+#        )""")
+#
+#     cur.execute("""CREATE TABLE IF NOT EXISTS lessons(
+#         id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         lesson_title TEXT NOT NULL
+#         )""")
 
-with sqlite3.connect("education.db") as con:
+
+# ========================== Занятие 32 от 29.06.2025 sql через Pycharm ==========================
+
+import sqlite3
+
+# cars_list = [
+#     ('BMW', 54000),
+#     ('Chevrolet', 46000),
+#     ('Daewoo', 38000),
+#     ('Citroen', 29000),
+#     ('Honda', 33000)
+# ]
+#
+# with sqlite3.connect("automobile.db") as con:
+#     cur = con.cursor()
+#     cur.execute("""
+#     CREATE TABLE IF NOT EXISTS cars(
+#         car_id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         model TEXT,
+#         price INTEGER
+#     )""")
+
+    # cur.execute("INSERT INTO cars VALUES(1, 'Renault', 22000)")
+    # cur.execute("INSERT INTO cars VALUES(2, 'Volvo', 29000)")
+    # cur.execute("INSERT INTO cars VALUES(3, 'Mercedes', 57000)")
+    # cur.execute("INSERT INTO cars VALUES(4, 'Bentley', 35000)")
+    # cur.execute("INSERT INTO cars VALUES(5, 'Audi', 52000)")
+
+    # for car in cars_list:
+    #     cur.execute("INSERT INTO cars VALUES(NULL, ?, ?)", car)  # ('BMW', 54000),
+
+    # cur.executemany("INSERT INTO cars VALUES(NULL, ?, ?)", cars_list)
+
+    # cur.execute("UPDATE cars SET price = :Price WHERE model LIKE 'B%'", {"Price": 0})
+
+    # cur.executescript("""
+    # DELETE FROM cars WHERE model LIKE 'B%';
+    # UPDATE cars SET price = price + 100;
+    # """)
+
+# con = None
+# try:
+#     con = sqlite3.connect("automobile.db")
+#     cur = con.cursor()
+#     cur.executescript("""
+#     BEGIN;
+#     INSERT INTO cars VALUES(NULL, 'Renault', 22000);
+#     UPDATE cars SET price = price + 100;
+#     """)
+#     con.commit()
+# except sqlite3.Error as e:
+#     if con:
+#         con.rollback()
+#     print("Ошибка выполнения запроса")
+# finally:
+#     if con:
+#         con.close()
+
+# with sqlite3.connect("automobile.db") as con:
+#     con.row_factory = sqlite3.Row
+#     cur = con.cursor()
+#     cur.executescript("""
+#     CREATE TABLE IF NOT EXISTS cars(
+#         car_id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         model TEXT,
+#         price INTEGER
+#     );
+#     CREATE TABLE IF NOT EXISTS cost(
+#         name TEXT, tr_in INTEGER, buy INTEGER
+#     );
+#     """)
+
+    # cur.execute("INSERT INTO cars VALUES(NULL, 'Запорожец', 1000)")
+    # last_id = cur.lastrowid
+    # buy_id = 2
+    # cur.execute("INSERT INTO cost VALUES('Илья', ?, ?)", (last_id, buy_id))
+
+    # cur.execute("SELECT model, price FROM cars")
+    # for res in cur:
+    #     print(res['model'], res['price'])
+    #
+    # print(cur.fetchone())
+    # print(cur.fetchmany(5))
+#     # print(cur.fetchall())
+#
+# def read_ava(n):
+#     try:
+#         with open(f"avatars/{n}.png", "rb") as f:
+#             return f.read()
+#     except IOError as e:
+#         print(e)
+#         return False
+#
+# def write_ava(name, data):
+#     try:
+#         with open(name, "wb") as f:
+#             f.write(data)
+#     except IOError as e:
+#         print(e)
+#     #     return False
+#     # return True
+#
+# with sqlite3.connect("automobile.db") as con:
+#     con.row_factory = sqlite3.Row
+#     cur = con.cursor()
+#     cur.executescript("""
+#     CREATE TABLE IF NOT EXISTS users(
+#         name TEXT,
+#         ava BLOB,
+#         score INTEGER
+#     );""")
+
+    # img = read_ava(1)
+    # if img:
+    #     binary = sqlite3.Binary(img)
+    #     cur.execute("INSERT INTO users VALUES('Илья', ?, 1000)", (binary,))
+
+    # cur.execute("SELECT ava FROM users")
+    # img = cur.fetchone()['ava']
+    # write_ava("out.png", img)
+
+# with sqlite3.connect("automobile.db") as con:
+#     cur = con.cursor()
+#     # print(con.iterdump())
+#
+#     for sql in con.iterdump():
+#         print(sql)
+
+# with sqlite3.connect("automobile.db") as con:
+#     cur = con.cursor()
+#
+#     with open("sql_dump.sql", 'w') as f:
+#         for sql in con.iterdump():
+#             f.write(sql)
+
+with sqlite3.connect("cars.db") as con:
     cur = con.cursor()
-    cur.execute("""CREATE TABLE IF NOT EXISTS student(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    surname TEXT NOT NULL,
-    name TEXT NOT NULL,
-    patronymic  TEXT NOT NULL,
-    age INTEGER NOT NULL CHECK(age >= 17 AND age <= 50),
-    [group] TEXT NOT NULL,
-    FOREIGN KEY ([group]) REFERENCES groups (id) ON DELETE RESTRICT
-    )""")
 
-    cur.execute("""CREATE TABLE IF NOT EXISTS groups(
-       id INTEGER PRIMARY KEY AUTOINCREMENT,
-       group_name TEXT NOT NULL)
-       """)
-
-    cur.execute("""CREATE TABLE IF NOT EXISTS association(
-       lesson_id INTEGER NOT NULL,
-       group_id INTEGER NOT NULL,
-       PRIMARY KEY (lesson_id, group_id),
-       FOREIGN KEY (lesson_id) REFERENCES lessons (id),
-       FOREIGN KEY (group_id) REFERENCES groups (id)
-       )""")
-
-    cur.execute("""CREATE TABLE IF NOT EXISTS lessons(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        lesson_title TEXT NOT NULL
-        )""")
-
+    with open("sql_dump.sql", "r") as f:
+        sql = f.read()
+        cur.executescript(sql)
